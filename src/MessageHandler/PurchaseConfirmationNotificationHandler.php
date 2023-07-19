739c1b1e90dfeab9cf07524cabc71f4f0357f3ac
Kacper <kacper.kubacki87@gmail.com>
2023-07-20 00:40:58 +0200

    Installed mailer and mpdf. Created pdf attach to email and send email

--- src/MessageHandler/PurchaseConfirmationNotificationHandler.php
+++ src/MessageHandler/PurchaseConfirmationNotificationHandler.php
@@ -3,15 +3,37 @@
 namespace App\MessageHandler;
 
 use App\Message\PurchaseConfirmationNotification;
+use Mpdf\Mpdf;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Mime\Email;
 
 #[AsMessageHandler]
 class PurchaseConfirmationNotificationHandler
 {
+    public function __construct(private MailerInterface $mailer)
+    {
+    }
+
     public function __invoke(PurchaseConfirmationNotification $notification)
     {
-        echo "Creating PDF contract note...<br>";
+//        1. Create a PDF contract note
+        $mpdf = new Mpdf();
+        $content = "<h1>Contract Note For Order {$notification->getOrder()->getId()}</h1>";
+        $content .= "<p> Total: <b>$12839.43</b></p>";
 
-        echo "Emaling contract note to ". $notification->getOrder()->getBuyer()->getEmail() . "<br>";
+        $mpdf->writeHtml($content);
+        $contractNotePdf = $mpdf->output('','S');
+
+
+//        2. Email the contract note to the buyer
+        $email = (new Email())
+            ->from('sales@stocksapp.com')
+            ->to($notification->getOrder()->getBuyer()->getEmail())
+            ->subject('Contract note for order ' . $notification->getOrder()->getId())
+            ->text('Here is your contract note')
+            ->attach($contractNotePdf, 'contract-note.pdf');
+
+        $this->mailer->send($email);
     }
 }
